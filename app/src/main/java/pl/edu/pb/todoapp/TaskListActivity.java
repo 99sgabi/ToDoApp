0990ffdc88285c698ae4d21ebbf9592403b1d690
99sgabi <99sgabi@wp.pl>
2020-12-10 21:17:54 +0100

    Zasoby sieciowe z lokalizacja :D

--- app/src/main/java/pl/edu/pb/todoapp/TaskListActivity.java
+++ app/src/main/java/pl/edu/pb/todoapp/TaskListActivity.java
@@ -2,20 +2,32 @@ package pl.edu.pb.todoapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,11 +37,39 @@ import retrofit2.Retrofit;
 public class TaskListActivity extends SingleFragmentActivity {
     private ClosesedCity closesedCity;
     private TextView messageBox;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     private void findLocation()
     {
-        //TODO: use here GPS or sth like that
-        String query = "53.08,23.08";
+        //checking permissions
+        if(ActivityCompat.checkSelfPermission(TaskListActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            //when permission is granted
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    //initialize location
+                    Location location = task.getResult();
+                    if(location != null)
+                    {
+                        // TODO: make sure String is correct . i , zwlaszcza
+                        double longitude = location.getLongitude();
+                        double latitude = location.getLatitude();
+                        String query = Double.toString(latitude) + "," + Double.toString(longitude);
+                        findClosesedCity(query);
+                    }
+                }
+            });
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(TaskListActivity.this , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    private void findClosesedCity(String query)
+    {
+        //String query = "53.08,23.08";
         WeatherService weatherService = WeatherApiInstance.getInstance().create(WeatherService.class);
         Call<List<ClosesedCity>> weatherApiCall = weatherService.findClosesedCities(query);
 
@@ -37,7 +77,7 @@ public class TaskListActivity extends SingleFragmentActivity {
             @Override
             public void onResponse(Call<List<ClosesedCity>> call, Response<List<ClosesedCity>> response) {
                 closesedCity = response.body().get(0);
-                showMessage();
+                    showMessage();
             }
 
             @Override
@@ -115,6 +155,7 @@ public class TaskListActivity extends SingleFragmentActivity {
         super.onCreate(savedInstanceState);
         startUp();
         setContentView(R.layout.task_list_activity);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         messageBox = findViewById(R.id.message_for_the_day);
         findLocation();
     }

7432a1cd3885b8635cecd0a5c39a5cd098fe3f5f
99sgabi <99sgabi@wp.pl>
2020-12-10 12:03:05 +0100

    Added Weather api with location

--- app/src/main/java/pl/edu/pb/todoapp/TaskListActivity.java
+++ app/src/main/java/pl/edu/pb/todoapp/TaskListActivity.java
@@ -10,15 +10,54 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class TaskListActivity extends SingleFragmentActivity {
+    private ClosesedCity closesedCity;
+    private TextView messageBox;
+
+    private void findLocation()
+    {
+        //TODO: use here GPS or sth like that
+        String query = "53.08,23.08";
+        WeatherService weatherService = WeatherApiInstance.getInstance().create(WeatherService.class);
+        Call<List<ClosesedCity>> weatherApiCall = weatherService.findClosesedCities(query);
+
+        weatherApiCall.enqueue(new Callback<List<ClosesedCity>>() {
+            @Override
+            public void onResponse(Call<List<ClosesedCity>> call, Response<List<ClosesedCity>> response) {
+                closesedCity = response.body().get(0);
+                messageBox.setText(closesedCity.city);
+            }
+
+            @Override
+            public void onFailure(Call<List<ClosesedCity>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void showMessage()
+    {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startUp();
+        setContentView(R.layout.task_list_activity);
+        messageBox = findViewById(R.id.message_for_the_day);
+        findLocation();
     }
 
     @Override
